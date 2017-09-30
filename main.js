c533b0a1ad7b74bff2549ffdbf7ede4b54f5cbe7
Tanmay Goel <goeltanmay@gmail.com>
2017-09-30 18:46:08 -0400

    log request

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
+  console.log(req);
   res.send('hello world')
 })
 
 var port = process.env.PORT || 3000;
 app.listen(port);
+console.log();
