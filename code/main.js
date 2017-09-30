f3ae5038aa287b00e0b2734d7b2c03b0af7120c0
Tanmay Goel <goeltanmay@gmail.com>
2017-09-30 18:33:52 -0400

    try heroku again

---
+++ code/main.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+var port = process.env.PORT || 3000;
+app.listen(port);
