8ede2cca1bd5ce39f735d3f5f526d93f1b5e9521
Tanmay Goel <goeltanmay@gmail.com>
2017-09-30 18:39:43 -0400

    changed to use heroku port

--- main.js
+++ main.js
@@ -6,6 +6,5 @@ app.get('/', function (req, res) {
   res.send('hello world')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var port = process.env.PORT || 3000;
+app.listen(port);
