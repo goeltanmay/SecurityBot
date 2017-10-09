1d0c9d2626a850dc65b9f2e71a1bee9a7c9e40af
Tanmay Goel <goeltanmay@gmail.com>
2017-10-09 16:56:24 -0400

    logging requests to api root

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,19 @@
 const reposController = require('../controllers').repos;
 
 module.exports = (app) => {
-  app.get('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the Todos API!',
-  }));
+  app.get('/api', (req, res) => {
+    console.log(req);
+    res.status(200).send({
+      message: 'Welcome to the Todos API!',
+    });
+  });
 
-  app.post('/api', (req, res) => res.status(200).send({
-    message: 'Welcome to the Todos API!',
-  }));
+  app.post('/api', (req, res) => {
+    console.log(req);
+    res.status(200).send({
+      message: 'Welcome to the Todos API!',
+    });
+  });
 
   app.post('/api/repos', reposController.create);
   app.get('/api/repos', reposController.list)
