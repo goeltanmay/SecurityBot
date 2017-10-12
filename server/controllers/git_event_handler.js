f60b01393858bcdc9f2a830bd5cfe3de38b03bf0
Palak Agrawal <pagrawa2@ncsu.edu>
2017-10-12 02:00:00 -0400

    installation repositories handler

--- server/controllers/git_event_handler.js
+++ server/controllers/git_event_handler.js
@@ -1,6 +1,23 @@
+function installation_repositories(req, res) {
+    Repo
+      .create({
+        username: req.body.sender.login,
+        repo: req.body.repositories_added.name,
+        instance_url: "skd",
+      })
+      .then(repo => RepoEvent
+              .create({
+                type: "installation_repositories",
+                detail: req.body.repo,
+                repoId: repo.id,
+            }))
+      .then(repoEvent => res.status(201).send(repo))
+      .catch(error => res.status(400).send(error));
+}
+
 function pull_request(req, res) {
     // do your stuff here
-    
+
 }
 
 module.exports = {
