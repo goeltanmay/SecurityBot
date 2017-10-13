fb5c696605b98c50e2a25b3739693708b1b61529
Palak Agrawal <pagrawa2@ncsu.edu>
2017-10-12 20:28:46 -0400

    get events

--- server/controllers/repos.js
+++ server/controllers/repos.js
@@ -1,44 +1,53 @@
-const Repo = require('../models').Repo;
-const RepoEvent = require('../models').RepoEvent;
+const Repo = require(‘../models’).Repo;
+const RepoEvent = require(‘../models’).RepoEvent;
 
 module.exports = {
-  create(req, res) {
-    // console.log(req.body);
-    return Repo
-      .create({
-        username: req.body.username,
-        repo: req.body.repo,
-        instance_url: req.body.instance_url,
-      })
-      .then(repo => res.status(201).send(repo))
-      .catch(error => res.status(400).send(error));
-  },
+ create(req, res) {
+   return Repo
+     .create({
+       username: req.body.username,
+       repo: req.body.repo,
+       instance_url: req.body.instance_url,
+     })
+     .then(repo => res.status(201).send(repo))
+     .catch(error => res.status(400).send(error));
+ },
 
-  list(req, res) {
-  return Repo
-  .findAll({
-    include: [{
-      model: RepoEvent,
-      as: 'repoEvents',
-    }],
-  })
-  .then(repos => res.status(200).send(repos))
-  .catch(error => res.status(400).send(error))
-  },
+ list(req, res) {
+   return Repo
+     .findAll({
+       include: [{
+         model: RepoEvent,
+         as: ‘repoEvents’,
+       }],
+     })
+   .then(repos => res.status(200).send(repos))
+   .catch(error => res.status(400).send(error))
+ },
 
-  getEvent(req, res) {
-    var events = Repo.findOne({
-      where:{
-        username: req.get('username'),
-        repo: req.get('repo'),
-      }
-    }).then( function (repo) {
-      if (!repo){
-        res.status(204).send();
-      }
-      res.status(200).send(repo.repoEvents[0]);
-    })
-    .catch(error => res.status(400).send(error))
-  }
-
+ getEvent(req, res) {
+   Repo.findOne({
+     where:{
+       username: req.params.username,
+       repo: req.params.repo,
+     },
+     include: [{
+       model: RepoEvent,
+       as: ‘repoEvents’,
+     }],
+   }).then( function (repo) {
+     if (!repo){
+       res.status(204).send();
+     }
+     else {
+       res.status(200).send(repo.repoEvents[0]);
+       RepoEvent.destroy({
+         where: {
+           id : repo.repoEvents[0].id
+         }
+       });
+     }
+   })
+   .catch(error => res.status(400).send(error))
+ }
 };
