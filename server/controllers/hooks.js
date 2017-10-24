34383e8e0a0d9d1f84832e4174ce6ea765669938
Palak Agrawal <pagrawa2@ncsu.edu>
2017-10-24 17:59:09 -0400

    email handled under same url as other events

--- server/controllers/hooks.js
+++ server/controllers/hooks.js
@@ -42,60 +42,62 @@ report = function (req, res) {
 	var repoName = req.body.repoName;
 	var detail = req.body.detail;
   var vulnerabilities = req.body.vulnerabilities.toString();
-	JWT.generateToken("5599")
-	.then(function (jwtToken) {
-		github.getInstallations(jwtToken)
-		.then(function (installations){
-			return github.getAccessTokensUrl(installations, userId);
-		})
-		.then(function(access_tokens_url) {
-			return github.postAccessTokenUrl(jwtToken, access_tokens_url);
-		})
-		.then(github.getToken)
-		.then(function (token) {
-      if (eventType === "installation_repositories") {
-        return github.createIssue(token, userId, repoName, vulnerabilities);
-      }
-      else if (eventType === "pull_request") {
-        return github.postCommentPullRequest(token, userId, repoName, detail, vulnerabilities);
-      }
-      else if (eventType === "push") {
-        return github.postCommentPush(token, userId, repoName, detail, vulnerabilities);
-      }
-		})
-		.then(function (res) {
-			return null;
-		});
-	});
-}
 
-emailReport = function(req, res) {
-  var transporter = nodemailer.createTransport({
-    service : 'Gmail',
-    auth: {
-           user: process.env.gmail_username, // Your email id
-           pass: process.env.gmail_password // Your password
-       }
-  });
+  if (eventType === "email_request") {
+    var transporter = nodemailer.createTransport({
+      service : 'Gmail',
+      auth: {
+             user: process.env.gmail_username, // Your email id
+             pass: process.env.gmail_password // Your password
+         }
+    });
 
-  var mailOptions = {
-    from: process.env.gmail_username, // sender address
-    to: req.body.detail, // list of receivers
-    subject: 'Robocop Report', // Subject line
-    text: req.body.vulnerabilities.toString() //, // plaintext body
-  };
-
-  transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-        res.json({yo: 'error'});
-    }else{
-        console.log('Message sent: ' + info.response);
-        res.json({yo: info.response});
+    var mailOptions = {
+      from: process.env.gmail_username, // sender address
+      to: detail, // list of receivers
+      subject: 'Robocop Report', // Subject line
+      text: vulnerabilities //, // plaintext body
     };
-  });
 
-  res.status(200).send();
+    transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          console.log(error);
+          res.json({yo: 'error'});
+      }else{
+          console.log('Message sent: ' + info.response);
+          res.json({yo: info.response});
+      };
+    });
+
+    res.status(200).send();
+  }
+	else {
+    JWT.generateToken("5599")
+  	.then(function (jwtToken) {
+  		github.getInstallations(jwtToken)
+  		.then(function (installations){
+  			return github.getAccessTokensUrl(installations, userId);
+  		})
+  		.then(function(access_tokens_url) {
+  			return github.postAccessTokenUrl(jwtToken, access_tokens_url);
+  		})
+  		.then(github.getToken)
+  		.then(function (token) {
+        if (eventType === "installation_repositories") {
+          return github.createIssue(token, userId, repoName, vulnerabilities);
+        }
+        else if (eventType === "pull_request") {
+          return github.postCommentPullRequest(token, userId, repoName, detail, vulnerabilities);
+        }
+        else if (eventType === "push") {
+          return github.postCommentPush(token, userId, repoName, detail, vulnerabilities);
+        }
+  		})
+  		.then(function (res) {
+  			return null;
+  		});
+  	});
+  }
 }
 
 email = function(req, res) {
@@ -127,7 +129,6 @@ module.exports = {
   setup,
   register,
   report,
-  emailReport,
   emailReportForm,
   email,
 }
