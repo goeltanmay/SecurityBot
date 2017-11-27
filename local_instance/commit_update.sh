77126b58833735b9012aac7dc72d3e6f339e908d
Jitin Nagpal <jkumar3@ncsu.edu>
2017-11-27 14:59:00 -0500

    upadted script to getjenkins env variable

--- local_instance/commit_update.sh
+++ local_instance/commit_update.sh
@@ -13,15 +13,15 @@ cd $repo_path
 pwd
 echo git checkout -b $new_branch $commitID
 # git checkout master
-git pull origin master
-git checkout -b $new_branch $commitID
+sudo git pull origin master
+sudo git checkout -b $new_branch $commitID
 echo bhanched_checked_out......
 # git --work-tree=/Users/jitinnagpal/Documents/workspace/PatientsApp checkout -b new_branchbeafb5d30989f2edbe1fde03669eeca08a6444e3 beafb5d30989f2edbe1fde03669eeca08a6444e3
 jenkins_path=$4
 JENKINS_URL=$6
 # echo $jenkins_path
 echo java -jar $jenkins_path -s $JENKINS_URL build $5
-java -jar $jenkins_path -s $JENKINS_URL build $5
+sudo java -jar $jenkins_path -s $JENKINS_URL build $5
 # cd E:
 # cd Repos/testing
 # git checkout -b $new_branch $commitID
